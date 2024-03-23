2c861c15373281e4f77f1634f12db4aefa281092
Ainur <sibrinat616@gmail.com>
2024-03-23 09:14:22 +0300

    git init

---
+++ parsingSite/parsingSite/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  parsingSite
+//
+//  Created by Ainur on 23.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ parsingSite/parsingSite/parsingSiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  parsingSiteApp.swift
+//  parsingSite
+//
+//  Created by Ainur on 23.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct parsingSiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
